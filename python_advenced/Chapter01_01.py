5727a41ba84cbe19d6be8dd17aa0cd28cf866bed
이하영 <lhy7574@naver.com>
2021-03-24 23:08:33 +0900

    210324 완료 - class & method 3,4

--- python_advenced/Chapter01_01.py
+++ python_advenced/Chapter01_01.py
@@ -74,3 +74,49 @@ del students_dicts[1]
 print(students_dicts)
 print()
 print()
+
+
+
+# 클래스 구조
+# 구조 설계 후 재사용성 증가, 코드 반복 최소화, 메소드 활용
+
+class Student():
+    def __init__(self, name, number, grade, details):
+        self._name = name
+        self._number = number
+        self._grade = grade
+        self._details = details
+
+    def __str__(self):
+        return 'str : {} - {}'.format(self._name, self._number)
+    # __str__이 없는 경우 __repr__ 호출
+    def __repr__(self):
+        return 'repr : {} - {}'.format(self._name, self._number)
+
+student1 = Student('Kim', 1, 1, {'gender': 'Male', 'score1': 95, 'score2': 88})
+student2 = Student('Lee', 2, 2, {'gender': 'Male', 'score1': 77, 'score2': 92})
+student3 = Student('Park', 3, 4, {'gender': 'Male', 'score1': 00, 'score2': 100})
+
+print(student1.__dict__)
+print(student2.__dict__)
+print(student3.__dict__)
+
+
+# 리스트 선언
+students_list = []
+
+students_list.append(student1)
+students_list.append(student2)
+students_list.append(student3)
+
+print()
+
+print(students_list)
+
+
+# 반복(__str__)
+for x in students_list:
+    print(repr(x))
+    print(x)
+
+
